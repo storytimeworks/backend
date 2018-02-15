8fc5a000cb46ed56fcc2ba10d5afbea406d6ce9a
Jack Cook <hello@jackcook.nyc>
2018-02-14 23:26:43 -0500

    Create example of a response from vocabulary endpoint

--- application.py
+++ application.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__,
-                    static_folder="build/static",
-                    template_folder="build")
+application = Flask(__name__)
+cors = CORS(application)
+application.config["CORS_HEADERS"] = "Content-Type"
 
-# add a rule for the index page.
-application.add_url_rule("/", "index", (lambda: render_template("index.html")))
+@application.route("/vocabulary")
+def vocabulary():
+    data = {
+        "source": "你好",
+        "pinyin": "nǐ hǎo",
+        "translation": "hello",
+        "definitions": [
+            {
+                "meaning": "(greeting)",
+                "source_sentence": "{你好}！我是沙拉。",
+                "target_sentence": "{Hello}! I'm Sarah."
+            }
+        ]
+    }
 
-# run the app.
+    return jsonify(data)
+
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
     application.debug = True
     application.run()
