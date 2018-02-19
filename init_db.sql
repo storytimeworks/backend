c46253bebadfa1cbd250c9d060ba421c95651694
Jack Cook <hello@jackcook.nyc>
2018-02-18 19:22:26 -0800

    Fixed MySQL character set issues

--- init_db.sql
+++ init_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE chinese_entries (
   source_is_chinese BOOLEAN NOT NULL,
   translations TEXT NOT NULL,
   PRIMARY KEY (id)
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 INSERT INTO chinese_entries (chinese, english, pinyin, source_is_chinese, translations) VALUES ("我", "I, me", "wǒ", 1, "1");
 INSERT INTO chinese_entries (chinese, english, pinyin, source_is_chinese, translations) VALUES ("是", "to be", "shì", 1, "2");
@@ -23,7 +23,7 @@ CREATE TABLE chinese_translations (
   chinese_sentence TEXT NOT NULL,
   english_sentence TEXT NOT NULL,
   PRIMARY KEY (id)
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-INSERT INTO chinese_translations (chinese, english, pinyin, definition, pos, chinese_sentence, english_sentence) VALUES ("我", "I", "wǒ", "(pronoun)", "pronoun", "{我}是沙拉。", "{I} am Sarah.");
-INSERT INTO chinese_translations (chinese, english, pinyin, definition, pos, chinese_sentence, english_sentence) VALUES ("是", "be", "shì", "(verb)", "verb", "我{是}沙拉。", "I {am} Sarah");
+INSERT INTO chinese_translations (chinese, english, pinyin, definition, pos, chinese_sentence, english_sentence) VALUES ("我", "I", "wǒ", "(pronoun)", "pronoun", "我是沙拉。", "I am Sarah.");
+INSERT INTO chinese_translations (chinese, english, pinyin, definition, pos, chinese_sentence, english_sentence) VALUES ("是", "be", "shì", "(verb)", "verb", "我是沙拉。", "I am Sarah");
