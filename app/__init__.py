e3c34828c3ac7bb92e7da0b422e54053d669d059
Jack Cook <hello@jackcook.nyc>
2018-02-24 12:01:05 -0800

    Move application callable into run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
-from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
-application = Flask(__name__)
-application.config.from_object("config")
+db = None
 
-cors = CORS(application)
-db = SQLAlchemy(application)
+def configure_app(app):
+    global db
 
-from app.mod_vocab.controllers import mod_vocab as vocab_module
-application.register_blueprint(vocab_module)
+    app.config.from_object("config")
 
-db.create_all()
+    cors = CORS(app)
+    db = SQLAlchemy(app)
+
+    from app.mod_vocab.controllers import mod_vocab as vocab_module
+    app.register_blueprint(vocab_module)
+
+    db.create_all()
--- run.py
+++ run.py
@@ -1,2 +1,8 @@
-from app import application
-application.run(debug=True)
+from flask import Flask
+from app import configure_app
+
+application = Flask(__name__)
+configure_app(application)
+
+if __name__ == "__main__":
+    application.run(debug=True)
