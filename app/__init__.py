4cde39468716c25dd0bb662088cb72daaa3184c7
Jack Cook <hello@jackcook.nyc>
2018-02-24 11:43:01 -0800

    Rename app to application for EB

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object("config")
+application = Flask(__name__)
+application.config.from_object("config")
 
-cors = CORS(app)
-db = SQLAlchemy(app)
+cors = CORS(application)
+db = SQLAlchemy(application)
 
 from app.mod_vocab.controllers import mod_vocab as vocab_module
-app.register_blueprint(vocab_module)
+application.register_blueprint(vocab_module)
 
 db.create_all()
