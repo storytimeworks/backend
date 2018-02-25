36fd70b75b721b3e2a3ee6ff439d7a1159d67e9a
Jack Cook <hello@jackcook.nyc>
2018-02-25 16:29:25 -0500

    Use pip module to redirect to HTTPS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import redirect, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_sslify import SSLify
 import os
 
 db = None
@@ -12,19 +13,12 @@ def configure_app(app):
 
     cors = CORS(app)
     db = SQLAlchemy(app)
+    sslify = SSLify(app, permanent=True)
 
     from app.mod_vocab.controllers import mod_vocab as vocab_module
     from app.mod_users.controllers import mod_users as users_module
 
     app.register_blueprint(vocab_module)
     app.register_blueprint(users_module)
-
-    if os.environ["ENVIRONMENT"] == "production":
-        @app.before_request
-        def before_request():
-            if not request.is_secure:
-                url = request.url.replace("http://", "https://", 1)
-                code = 301
-                return redirect(url, code=code)
 
     db.create_all()
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 from app import configure_app
 
 # Include application callable here so it can be used by WSGI
@@ -7,4 +9,5 @@ configure_app(application)
 
 # Run the application if it's being called by Elastic Beanstalk server
 if __name__ == "__main__":
-    application.run(debug=True)
+    is_production = os.environ["ENVIRONMENT"] == "production"
+    application.run(debug=not is_production)
