db173ce68c7980e17364db6b8127e1051e5864fb
Jack Cook <hello@jackcook.nyc>
2018-03-11 11:07:51 -0400

    Set development environment variables with command line option

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 from flask import Flask
-import os
+import os, sys
 
 from app import configure_app
+
+if len(sys.argv) > 1 and sys.argv[1] == "dev":
+    os.environ["ENVIRONMENT"] = "dev"
+    os.environ["RDS_DB_NAME"] = "storytime"
+    os.environ["RDS_HOSTNAME"] = "localhost"
+    os.environ["RDS_PASSWORD"] = ""
+    os.environ["RDS_USERNAME"] = "root"
+    os.environ["SECRET_KEY"] = "secret"
 
 # Include application callable here so it can be used by WSGI
 application = Flask(__name__)
