8de3ff2631f1552d694e564a8f2ab3014d831c39
Jack Cook <hello@jackcook.nyc>
2018-03-12 21:25:07 -0400

    Added endpoint for English speech synthesis

--- app/mod_speech/models.py
+++ app/mod_speech/models.py
@@ -6,18 +6,21 @@ class Base(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
-class SpeechSynthesis(Base):
-
-    __tablename__ = "chinese_speech"
-
     source = db.Column(db.Text, nullable=False)
     filename = db.Column(db.String(40), nullable=False)
     voice = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def __init__(self, source, filename, voice):
         self.source = source
         self.filename = filename
         self.voice = voice
+
+class ChineseSpeechSynthesis(Base):
+
+    __tablename__ = "chinese_speech"
+
+class EnglishSpeechSynthesis(Base):
+
+    __tablename__ = "english_speech"
