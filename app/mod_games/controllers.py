4d1c7e4e49c4fade36c303784e992b447692137a
Jack Cook <hello@jackcook.nyc>
2018-05-08 22:05:45 -0400

    Wrote endpoint to get the next entry for flashcard game

--- app/mod_games/controllers.py
+++ app/mod_games/controllers.py
@@ -6,6 +6,7 @@ from app import db
 from app.mod_games import check_body
 import app.mod_games.errors as errors
 from app.mod_games.models import Attempt
+from app.mod_vocab.models import Entry
 
 mod_games = Blueprint("games", __name__, url_prefix="/games")
 
@@ -25,3 +26,21 @@ def log_attempt():
     db.session.commit()
 
     return jsonify(attempt.serialize())
+
+@mod_games.route("/flashcard/next", methods=["GET"])
+def get_next_entry():
+    if not current_user.is_active:
+        return errors.not_authenticated()
+
+    entry_id = random.randint(1, 240)
+    entry = Entry.query.filter_by(id=entry_id).first()
+    entry_data = entry.serialize()
+
+    attempt = Attempt.query.filter_by(entry_id=entry_id, user_id=current_user.id).first()
+
+    if not attempt:
+        entry_data["new"] = True
+    else:
+        entry_data["new"] = False
+
+    return jsonify(entry_data)
