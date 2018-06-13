0d51c10c6807da35a72614955d22863c4b083eee
Jack Cook <hello@jackcook.nyc>
2018-06-12 20:40:53 -0400

    Fixed punctuation issues with pinyin display

--- app/mod_passages/controllers.py
+++ app/mod_passages/controllers.py
@@ -98,8 +98,29 @@ def get_passage(passage_id):
                 flattened_pinyin_words = [[j for i in words for j in i] for words in pinyin_words]
                 joined_pinyin_words = ["".join(words) for words in flattened_pinyin_words]
 
-                for i, word in enumerate(words):
-                    words[i]["pinyin"] = joined_pinyin_words[i]
+                # True if the next word in the loop needs to be capitalized
+                capitalize_next_word = True
+
+                for i, _ in enumerate(words):
+                    word = joined_pinyin_words[i]
+
+                    if capitalize_next_word:
+                        # Capitalize this word, don't capitalize the next one
+                        word = word.title()
+                        capitalize_next_word = False
+
+                    # Replace Chinese punctutation with regular punctuation
+                    if word == "。":
+                        capitalize_next_word = True
+                        word = "."
+                    elif word == "！":
+                        capitalize_next_word = True
+                        word = "!"
+                    elif word == "？":
+                        capitalize_next_word = True
+                        word = "?"
+
+                    words[i]["pinyin"] = word
 
                 # Add the words to the passage data
                 passage_data["data"]["components"][idx]["words"] = words
