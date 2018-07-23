9acff3985fa7ec0f9341c1cdd2bdf6509a171532
Jack Cook <hello@jackcook.nyc>
2018-07-22 22:20:45 -0400

    Put Bing speech API key in an environment variable

--- app/mod_games/mod_speaker/controllers.py
+++ app/mod_games/mod_speaker/controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-import requests
+import os, requests
 
 mod_speaker_game = Blueprint("speaker_game", __name__, url_prefix="/games/speaker")
 
@@ -15,7 +15,7 @@ def check_answer():
 
     headers = {
         "Content-Type": "audio/wav; codec=audio/pcm; samplerate=16000",
-        "Ocp-Apim-Subscription-Key": "78de25c194ae468d8f2b6fde39294963"
+        "Ocp-Apim-Subscription-Key": os.environ["BING_SPEECH_API_KEY"]
     }
 
     r = requests.post(url, data=audio_data, headers=headers)
