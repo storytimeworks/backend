ed0007970eddedd1707cb3a8d6e5aa7abaabe48f
Jack Cook <hello@jackcook.nyc>
2018-07-24 17:40:03 -0400

    Added correct ranges to copy edit serialization

--- app/mod_games/mod_copy_edit/models.py
+++ app/mod_games/mod_copy_edit/models.py
@@ -21,12 +21,26 @@ class CopyEditQuestion(Question):
         self.correct_sentence = correct_sentence
 
     def serialize(self):
+        start_range = 0
+        end_range = 0
+
+        try:
+            start_range = self.prompt.index("{")
+            end_range = self.prompt.index("}") - 1
+        except ValueError:
+            pass
+
+        prompt_text = self.prompt.replace("{", "").replace("}", "")
+        words = segment(prompt_text)
+
         return {
             "id": self.id,
             "prompt": self.prompt,
             "explanation": self.explanation,
             "correct_sentence": self.correct_sentence,
-            "words": [word for word in segment(self.prompt)],
+            "start_range": start_range,
+            "end_range": end_range,
+            "words": words,
             "created_at": self.created_at,
             "updated_at": self.updated_at
         }
