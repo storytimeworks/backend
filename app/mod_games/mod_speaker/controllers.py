b08dd108b7117e63419a170e4f42d5eb5b98319c
Jack Cook <hello@jackcook.nyc>
2018-08-08 16:21:40 -0400

    Use detailed Speech Recognition results and upload audio to S3

--- app/mod_games/mod_speaker/controllers.py
+++ app/mod_games/mod_speaker/controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-import json, numpy as np, os, requests
+import boto3, json, numpy as np, os, requests, uuid
 
 from app import admin_required, db
 from app.mod_games.mod_speaker.models import SpeakerQuestion, SpeakerResult
@@ -24,11 +24,14 @@ def play_game(words=None):
 @mod_speaker_game.route("/check", methods=["POST"])
 @login_required
 def check_answer():
+    # Retrieve audio and answer data from the request
+    answer = request.args.get("answer")
     audio_data = request.files["file"].read()
 
+    # Send this audio to Bing's Speech Recognition API
     url = "https://speech.platform.bing.com/speech/recognition/dictation/cognitiveservices/v1" + \
         "?language=zh-CN" + \
-        "&format=simple"
+        "&format=detailed"
 
     headers = {
         "Content-Type": "audio/wav; codec=audio/pcm; samplerate=16000",
@@ -38,7 +41,39 @@ def check_answer():
     r = requests.post(url, data=audio_data, headers=headers)
     r.encoding = "utf-8"
 
-    return r.text
+    # Parse the results of the request
+    correct = False
+    data = r.json()
+    status = data["RecognitionStatus"]
+
+    if status == "Success":
+        # Loop through results and see if any have high enough confidence
+        for result in data["NBest"]:
+            if result["Lexical"] == answer and result["Confidence"] >= 0.75:
+                correct = True
+
+        # Upload the audio to S3 for training later
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=os.environ["S3_AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["S3_AWS_SECRET_ACCESS_KEY"]
+        )
+
+        filename = str(uuid.uuid4())
+        path = "speaker/%s/wrong/%s.wav" % (answer, filename)
+
+        if correct:
+            path = "speaker/%s/%s.wav" % (answer, filename)
+
+        s3.put_object(Body=audio_data, Bucket="storytimeai", Key=path)
+
+    # Create a result JSON object to send back to the browser
+    result = {
+        "correct": correct,
+        "status": status
+    }
+
+    return jsonify(result)
 
 @mod_speaker_game.route("/questions", methods=["GET"])
 @admin_required
